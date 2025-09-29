a36c3ab18c8ab7bf6b53a7a3351e95156c59b215
jianweijia <elonjia@umich.edu>
2025-09-29 03:10:32 -0400

    update

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -81,7 +81,7 @@ ninja.data = [
   {%- for collection in site.collections -%}
     {%- if collection.label != 'posts' -%}
       {%- for item in collection.docs -%}
-        {%- if p.nav -%}
+        {%- if p.nav or p.collection == "publications" -%}
           {
             {%- if item.inline -%}
               {%- assign title = item.content | newline_to_br | replace: "<br />", " " | replace: "<br/>", " " | strip_html | strip_newlines | escape | strip -%}
